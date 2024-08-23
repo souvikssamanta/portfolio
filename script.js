fee21953684cc4af75a313db1fea15991179c8fb
souviksamanta <souvikss2004@gmail.com>
2024-08-24 00:09:37 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var a=document.querySelector("body")   
+a.addEventListener("scroll",function(){
+var nav=document.querySelector(".main")
+nav.toggle("sticky",a.scroll)
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
